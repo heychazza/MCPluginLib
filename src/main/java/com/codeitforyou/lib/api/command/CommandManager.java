00ab4f73e1a81fe839e5c08586a97856ac14a29d
Charlie <me@chazza.net>
2020-01-27 19:39:42 +0000

    Further refactoring (again).

--- src/main/java/com/codeitforyou/lib/api/command/CommandManager.java
+++ src/main/java/com/codeitforyou/lib/api/command/CommandManager.java
@@ -7,11 +7,8 @@ import org.bukkit.command.CommandMap;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.PluginCommand;
 import org.bukkit.entity.Player;
-import org.bukkit.plugin.Plugin;
-import org.bukkit.plugin.SimplePluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
@@ -40,8 +37,8 @@ public class CommandManager {
             bukkitCommandMap.setAccessible(true);
             CommandMap commandMap = (CommandMap) bukkitCommandMap.get(Bukkit.getServer());
 
-            commandMap.register("seen", command);
-        } catch(Exception e) {
+            commandMap.register(aliases.get(0), command);
+        } catch (Exception e) {
             plugin.getLogger().warning("Failed to register command. Reason: " + e.getLocalizedMessage());
         }
 
